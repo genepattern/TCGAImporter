edb59ffb5476eb6e5af5564afc80250862cb9e0d
edjuaro <edjuaro@gmail.com>
2018-09-07 14:34:43 -0700

    Removing duplicate genes after translation and adding some extra data for testing

--- src/dfm_command_line_call.py
+++ src/dfm_command_line_call.py
@@ -17,7 +17,13 @@ INPUT_FILE_DIRECTORIES = os.path.join(ROOT, 'data/')
 command_line = "python "+TASKLIB+"download_from_manifest.py"\
                 + " -m " + INPUT_FILE_DIRECTORIES+"LIHC_MANIFEST.txt"\
                 + " -n " + INPUT_FILE_DIRECTORIES+"LIHC_METADATA.json"\
-                + " -g True " + "-c True " + "-t False " + "-o demo"
+                + " -g True " + "-c True " + "-t True " + "-o demo"
+
+# # # Debugging on 2018-09-06
+# command_line = "python "+TASKLIB+"download_from_manifest.py"\
+#                 + " -m " + INPUT_FILE_DIRECTORIES+"gdc_minifest_20171221_005438.txt"\
+#                 + " -n " + INPUT_FILE_DIRECTORIES+"metadata.cart.2017-12-21T21_41_22.870798.json"\
+#                 + " -g True " + "-c True " + "-t True " + "-o demo"
 
 print("About to call the module using the command line:", command_line)
 
