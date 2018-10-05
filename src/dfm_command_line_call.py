d923ba6f251a7285810af1e67e12290c9a455040
edjuaro <edjuaro@gmail.com>
2018-10-05 10:58:14 -0700

    updating the gp_unit_test gct file

--- src/dfm_command_line_call.py
+++ src/dfm_command_line_call.py
@@ -14,16 +14,16 @@ INPUT_FILE_DIRECTORIES = os.path.join(ROOT, 'data/')
 #                 # + " -d"
 
 # Debugging on 2018-05-17
-# command_line = "python "+TASKLIB+"download_from_manifest.py"\
-#                 + " -m " + INPUT_FILE_DIRECTORIES+"LIHC_MANIFEST.txt"\
-#                 + " -n " + INPUT_FILE_DIRECTORIES+"LIHC_METADATA.json"\
-#                 + " -g True " + "-c True " + "-t True " + "-o demo"
+command_line = "python "+TASKLIB+"download_from_manifest.py"\
+                + " -m " + INPUT_FILE_DIRECTORIES+"LIHC_MANIFEST.txt"\
+                + " -n " + INPUT_FILE_DIRECTORIES+"LIHC_METADATA.json"\
+                + " -g True " + "-c True " + "-t True " + "-o demo"
 
 # # # Debugging on 2018-09-06
-command_line = "python "+TASKLIB+"download_from_manifest.py"\
-                + " -m " + INPUT_FILE_DIRECTORIES+"gdc_minifest_20171221_005438.txt"\
-                + " -n " + INPUT_FILE_DIRECTORIES+"metadata.cart.2017-12-21T21_41_22.870798.json"\
-                + " -g True " + "-c True " + "-t True " + "-o demo"
+# command_line = "python "+TASKLIB+"download_from_manifest.py"\
+#                 + " -m " + INPUT_FILE_DIRECTORIES+"gdc_minifest_20171221_005438.txt"\
+#                 + " -n " + INPUT_FILE_DIRECTORIES+"metadata.cart.2017-12-21T21_41_22.870798.json"\
+#                 + " -g True " + "-c True " + "-t True " + "-o demo"
 
 print("About to call the module using the command line:", command_line)
 
